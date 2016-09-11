a77aa8ba9a3c9d92c6ee8c06049acd160e392be8
Aaron Marasco <aaronm67@gmail.com>
2016-09-11 18:10:51 -0400

    update test case, make slightly more efficient

--- merge-sort.js
+++ merge-sort.js
@@ -1,6 +1,8 @@
 // Given two sorted arrays, merges the two into a single sorted array
 // Works in O(N) time, O(N) space.
 module.exports = function(arr1, arr2) {
+  arr1 = arr1.slice(0);
+  arr2 = arr2.slice(0);
   var i = 0;
   var j = 0;
 
@@ -11,24 +13,12 @@ module.exports = function(arr1, arr2) {
     var val2 = arr2[j];
 
     if (val1 <= val2) {
-      newArr.push(val1);
-      i++;
+      newArr.push(arr1.shift());
     }
     else {
-      newArr.push(val2);
-      j++;
+      newArr.push(arr2.shift());
     }
   }
 
-  while (i < arr1.length) {
-    newArr.push(arr1[i]);
-    i++;
-  }
-
-  while (j < arr2.length) {
-    newArr.push(arr2[j]);
-    j++;
-  }
-
-  return newArr;
+  return newArr.concat(arr1).concat(arr2);
 }
--- test/merge-sort-spec.js
+++ test/merge-sort-spec.js
@@ -3,15 +3,54 @@ var should = require('chai').should()
 var expect = require('chai').expect;
 var mergeSort = require('../merge-sort.js');
 
+var intSort = function(a, b) {
+    if (a > b) return 1;
+    if (b > a) return -1;
+    return 0;
+};
+
 describe('merge-sort', function() {
   it('should output a single sorted array from two sorted arrays', function() {
     var list1 = [3, 4, 6, 10, 11, 15];
     var list2 = [1, 5, 8, 12, 14, 19];
-    var sorted = mergeSort(list1, list2)
-    sorted.should.deep.equal(list1.concat(list2).sort(function(a, b) {
-      if (a > b) return 1;
-      if (b > a) return -1;
-      return 0;
-    }));
+    var sorted = mergeSort(list1, list2);
+
+    console.log(sorted);
+
+    sorted.should.deep.equal(list1.concat(list2).sort(intSort));
+  });
+
+  it('shouldnt bomb on odd inputs', function() {
+    var list1 = [];
+    var list2 = [];
+
+    var sorted = mergeSort(list1, list2);
+    sorted.should.deep.equal([]);
+
+    list1 = [ -1, 2, 9999999];
+    list2 = [];
+    sorted = mergeSort(list1, list2);
+    sorted.should.deep.equal([ -1, 2, 9999999]);
+
+    var count = 10000;
+    list1 = [];
+    list2 = [];
+    while (count > 0) {
+      list1.push(Math.random());
+      list2.push(Math.random() / 2);
+      count--;
+    }
+
+    list1 = list1.sort();
+    list2 = list2.sort();
+
+    console.time('native sort');
+    var nativeSort = list1.concat(list2).sort(intSort);
+    console.timeEnd('native sort');
+
+    console.time('merge sort');
+    sorted = mergeSort(list1, list2);
+    sorted.should.deep.equal(nativeSort);
+    console.timeEnd('merge sort');
   });
 });
